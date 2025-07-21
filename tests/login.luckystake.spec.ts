c5fae8c318ba5e8b9e46d7e1ccae45284c44d330
Daniil Kolotii <daniilkolotij6@gmail.com>
2025-07-21 23:30:35 +0300

    deshi basara

--- tests/login.luckystake.spec.ts
+++ tests/login.luckystake.spec.ts
@@ -3,21 +3,21 @@ import { LoginPage } from '../pages/LoginPage';
 import { HomePage } from '../pages/HomePage';
 import { delay5Seconds } from './utils';
 
-const iPhone15 = {
-  name: 'iPhone 15',
-  userAgent:
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
-  viewport: { width: 393, height: 852 },
-  deviceScaleFactor: 3,
-  isMobile: true,
-  hasTouch: true,
-  defaultBrowserType: 'chromium',
-};
+//const iPhone15 = {
+//   name: 'iPhone 15',
+//   userAgent:
+//     'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+//   viewport: { width: 393, height: 852 },
+//   deviceScaleFactor: 3,
+//   isMobile: true,
+//   hasTouch: true,
+//   defaultBrowserType: 'chromium',
+// };
 
 const test = base.extend<{}>({
   context: async ({ browser }, use) => {
     const context = await browser.newContext({
-      ...iPhone15,
+      //...iPhone15,
       httpCredentials: {
         username: 'luckystake',
         password: 'luckystake1!',
